1327ff42c909b59fc75b169f6a36c1b9f732f4f0
ChungZinh <phamtrungvinh642002@gmail.com>
2024-07-22 21:20:33 +0700

    server: update get list

--- server/src/routes/category.route.js
+++ server/src/routes/category.route.js
@@ -3,10 +3,10 @@ const router = express.Router();
 const { asyncHandler } = require("../helpers/asyncHandler");
 const CategoryController = require("../controllers/category.controller");
 
+router.get("/get-list", asyncHandler(CategoryController.getList));
 
 router.use(require("../auth/authUtils").verifyToken);
 router.post("/create", asyncHandler(CategoryController.create));
-router.get("/get-list", asyncHandler(CategoryController.getList));
 router.put("/update/:id", asyncHandler(CategoryController.update));
 router.delete("/delete/:id", asyncHandler(CategoryController.delete));
 module.exports = router;
--- server/src/routes/product.route.js
+++ server/src/routes/product.route.js
@@ -2,10 +2,9 @@ const express = require("express");
 const router = express.Router();
 const { asyncHandler } = require("../helpers/asyncHandler");
 const ProductController = require("../controllers/product.controller");
-
+router.get("/get-products-list", asyncHandler(ProductController.getProductsList));
 router.use(require("../auth/authUtils").verifyToken);
 router.post("/create", asyncHandler(ProductController.createProduct));
-router.get("/get-products-list", asyncHandler(ProductController.getProductsList));
 router.put("/move-to-draft/:id", asyncHandler(ProductController.moveToDraft));
 router.put("/publish-product/:id", asyncHandler(ProductController.publishProduct));
 router.put("/update-product/:id", asyncHandler(ProductController.updateProduct));
