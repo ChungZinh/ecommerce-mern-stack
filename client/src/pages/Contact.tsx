f20fb4040c65c7146663168f2e819919e8ea0638
ChungZinh <phamtrungvinh642002@gmail.com>
2024-07-24 23:45:18 +0700

    client: add send mail contact page

--- client/src/pages/Contact.tsx
+++ client/src/pages/Contact.tsx
@@ -1,8 +1,46 @@
-import { Button, Textarea, TextInput } from "flowbite-react";
+import { Button, Spinner, Textarea, TextInput } from "flowbite-react";
 import map from "../assets/images/map.png";
 import { HiLocationMarker } from "react-icons/hi";
 import contact_user from "../assets/images/contact_user.png";
+import { useState } from "react";
+interface FormData {
+  name: string;
+  email: string;
+  phone: string;
+  subject: string;
+  message: string;
+}
 export default function Contact() {
+  const [formData, setFormData] = useState({} as FormData);
+  const [loading, setLoading] = useState(false);
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setLoading(true);
+    try {
+      await fetch("http://localhost:3000/api/contact/send-mail", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      setFormData({
+        name: "",
+        email: "",
+        phone: "",
+        subject: "",
+        message: "",
+      });
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
+
   return (
     <div className="lg:max-w-screen-2xl  mx-auto md:w-full px-4 mt-20">
       <div className="lg:max-w-screen-xl mx-auto flex gap-4">
@@ -108,15 +146,40 @@ export default function Contact() {
               Your email address will not be published. Required fields are
               marked *
             </p>
-            <form className="space-y-4">
+            <form onSubmit={handleSubmit} className="space-y-4">
               <div className="grid lg:grid-cols-2 md:grid-cols-1 gap-4">
-                <TextInput placeholder="Your Name" />
-                <TextInput placeholder="Your Email" />
-                <TextInput placeholder="Your Phone" />
-                <TextInput placeholder="Subject" />
+                <TextInput
+                  placeholder="Your Name"
+                  id="name"
+                  onChange={handleChange}
+                />
+                <TextInput
+                  placeholder="Your Email"
+                  id="email"
+                  onChange={handleChange}
+                />
+                <TextInput
+                  placeholder="Your Phone"
+                  id="phone"
+                  onChange={handleChange}
+                />
+                <TextInput
+                  placeholder="Subject"
+                  id="subject"
+                  onChange={handleChange}
+                />
               </div>
-              <Textarea rows={"4"} />
-              <Button type="submit">Send Message</Button>
+              <Textarea rows={"4"} id="message" onChange={handleChange} />
+              <Button type="submit">
+                {loading ? (
+                  <div className="">
+                    <Spinner size={"sm"} />
+                    <span className="pl-3">Loading...</span>
+                  </div>
+                ) : (
+                  "Send Message"
+                )}
+              </Button>
             </form>
           </div>
           <div className="w-1/3 flex justify-end items-center">
